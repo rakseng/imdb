01e77c1c8f022b5e21e4ebb1afa6ce72fb25e3de
rakseng <mrakseng@gmail.com>
2023-04-17 03:13:37 +0600

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,32 @@
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex molestias
+        tempore commodi quia voluptas illum officia repellendus debitis.
+        Doloremque, omnis vero. Minima quasi nesciunt numquam eaque hic
+        quibusdam commodi, laboriosam ullam ea, provident at ut accusantium.
+      </p>
+      <p>
+        Aut pariatur aliquam adipisci, nulla, minima ab voluptatem consequuntur
+        harum dicta illum accusamus, ipsam culpa fugit! Harum commodi
+        consectetur, expedita totam similique nemo facere accusantium ea neque
+        in non ratione doloremque omnis provident eos numquam est corrupti eum
+        aliquam deserunt, blanditiis at ad animi vero. Dolorem repudiandae earum
+        delectus iusto dolores quibusdam, mollitia
+      </p>
+      <p>
+        recusandae quasi totam accusantium nisi natus, officiis libero nesciunt
+        facere similique alias non ratione blanditiis laborum. Doloremque
+        placeat magni esse cum quasi incidunt? Ex quas rerum neque corrupti
+        minima tempora fugit eum iusto maxime ab possimus, cupiditate distinctio
+        deleniti nam dolores aperiam, accusamus atque enim ratione sunt
+        doloremque id soluta? Quibusdam doloremque, deserunt odit veniam dolorem
+        nostrum sit reprehenderit?
+      </p>
+    </div>
+  );
+};
+
+export default About;
